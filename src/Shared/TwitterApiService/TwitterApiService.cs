19a246c6691cce4190855a995da545125470ca27
Adrian <70015674+adriansjens@users.noreply.github.com>
2020-08-22 18:27:42 +0200

    addded twitter apiService skeleton

--- src/Shared/TwitterApiService/TwitterApiService.cs
+++ src/Shared/TwitterApiService/TwitterApiService.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
-namespace Shared1.TwitterApiService
+namespace Shared.TwitterApiService
 {
     //Todo
     public class TwitterApiService
